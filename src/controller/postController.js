43ba2afd12ff85d309bdd41ee539c254ca8efba7
veronicalupinacci <116969384+veronicalupinacci@users.noreply.github.com>
2023-11-17 19:30:51 -0500

    Login backend

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -1,5 +1,5 @@
 // Import the database functions
-const { createUser, isUsernameAvaliable, isEmailAvaliable } = require('../util/dbFunctions');
+const { createUser, isUsernameAvaliable, isEmailAvaliable, loginDB } = require('../util/dbFunctions');
 
 // Define the route handler for creating a user account
 const createAccount = async (req, res) => {
@@ -19,7 +19,7 @@ const createAccount = async (req, res) => {
             return res.status(400).json({ message: 'Email is not available' });
         }
 
-        // Use the database functions to save the user data
+        // Use your database functions to save the user data
         const result = await createUser(username, email, password);
 
         // Send a response back to the client
@@ -30,7 +30,28 @@ const createAccount = async (req, res) => {
     }
 };
 
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await loginDB(email, password);
+
+        if (user.length > 0) {
+            // User authentication successful
+            console.log(`User ${user[0].Username} authenticated successfully.`);
+            res.status(200).json({ message: 'Login successful', user: user[0] });
+        } else {
+            // User not found or invalid credentials
+            res.status(401).json({ message: 'Invalid email or password.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
 // Export the route handler as a function
 module.exports = {
     createAccount: createAccount,
+    login: login
 };
--- src/util/dbFunctions.js
+++ src/util/dbFunctions.js
@@ -12,17 +12,28 @@ const createUser = async (username, email, userPassword) => {
     }
 };
 
+const loginDB = async (email, userPassword) => {
+    try {
+        const sql = 'SELECT * FROM UserRegistration WHERE Email = ? AND UserPassword = ?';
+        const [rows, fields] = await pool.execute(sql, [email, userPassword]);
+        return rows
+    }
+    catch (error) {
+        throw error;
+    }
+};
+
 /// Function to check if the username is already in the database
-const isUsernameAvaliable = async (username) => {
+async function isUsernameAvaliable(username) {
     try {
         const sql = 'SELECT * FROM UserRegistration WHERE Username = ?';
         const [rows, fields] = await pool.execute(sql, [username]);
         // Return true if the username is available
-        return rows.length === 0; 
+        return rows.length === 0;
     } catch (error) {
         throw error;
     }
-};
+}
 
 // Function to check if the email is already in the database
 const isEmailAvaliable = async (email) => {
@@ -40,5 +51,6 @@ const isEmailAvaliable = async (email) => {
 module.exports = {
     createUser: createUser,
     isUsernameAvaliable: isUsernameAvaliable,
-    isEmailAvaliable: isEmailAvaliable
+    isEmailAvaliable: isEmailAvaliable,
+    loginDB: loginDB
 };
