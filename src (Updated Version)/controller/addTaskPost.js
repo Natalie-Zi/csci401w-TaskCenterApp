ebacf3fa6ddae453f394dbec7d4cebb91b01bc49
BrayanEscobar11 <143458363+BrayanEscobar11@users.noreply.github.com>
2023-12-12 15:09:24 -0500

    Add files via upload

---
+++ src (Updated Version)/controller/addTaskPost.js
@@ -0,0 +1,95 @@
+const { addTaskDB, checkViewPermission, getCalendarIDByName, getTasksForCalendar  } = require('../util/addTaskQueries');
+const { isCalendarOwnedByUser, isCalendarSharedWithUser} = require('../util/shareCalendar');
+
+const addTask = async (req, res) => {
+    try {
+        const { taskTitle, dueDate, dueTime, calendarName } = req.body;
+        // Debug: Log the content of req.body
+        console.log('                       ');
+        console.log('Request Body:', req.body);
+
+        // Get the UserID from the session
+        const loggedInUserID = req.session.userId;
+
+        // Ensure the user is logged in
+        if (!loggedInUserID) {
+            console.log('User not logged in.');
+            return res.status(401).json({ message: 'User not logged in.' });
+        }
+
+        // Retrieve calendar ID associated with the calendar name
+        const calendarID = await getCalendarIDByName(calendarName);
+        console.log('Calendar ID retrieved:', calendarID);
+
+        // Check if the logged-in user owns the calendar or has edit permisson
+        const ownedByUser = await isCalendarOwnedByUser(loggedInUserID, calendarName);
+        const sharedWithUser = await isCalendarSharedWithUser(loggedInUserID, calendarID);
+
+        // If the calendar is not owned by the user and has no edit permisson
+        if (!ownedByUser && !sharedWithUser) {
+            return res.status(403).json({ message: 'Unauthorized. Calendar not accessible.' });
+        }
+
+        const hasviewPermission = await checkViewPermission(loggedInUserID, calendarID);
+        if (hasviewPermission) {
+            return res.status(403).json({ message: 'Unauthorized. Not allowed to add task because only have View permission.' });
+        }
+
+        // Use the retrieved calendarID and loggedInUserID to add the task to the database
+        const result = await addTaskDB(taskTitle, dueDate, dueTime, calendarID, loggedInUserID);
+
+        console.log('Task added successfully.');
+        res.status(201).json({ message: 'Task added successfully.', data: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Define route to handle get information for calendarName
+const getTaskInformation = async (req, res) => {
+    try {
+        const { calendarName } = req.body;
+
+        // Get the UserID from the session
+        const loggedInUserID = req.session.userId;
+
+        // Ensure the user is logged in
+        if (!loggedInUserID) {
+            console.log('User not logged in.');
+            return res.status(401).json({ message: 'User not logged in.' });
+        }
+
+        // Retrieve calendar ID associated with the calendar name
+        const calendarID = await getCalendarIDByName(calendarName);
+
+        if (!calendarID) {
+            return res.status(404).json({ message: 'Calendar not found.' });
+        }
+
+        // Check if the logged-in user owns the calendar or it's shared with them
+        const ownedByUser = await isCalendarOwnedByUser(loggedInUserID, calendarName);
+        const sharedWithUser = await isCalendarSharedWithUser(loggedInUserID, calendarID);
+
+        // If the calendar is not owned by the user and not shared with them
+        if (!ownedByUser && !sharedWithUser) {
+            return res.status(403).json({ message: 'Unauthorized. Calendar not accessible.' });
+        }
+
+        // Retrieve tasks associated with the calendar for the logged-in user
+        const tasks = await getTasksForCalendar(calendarID);
+
+        res.status(200).json({ tasks });
+        console.log('Tasks retrieved successfully for', calendarName);
+        console.log(tasks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+// Export the route handler as a function
+module.exports = {
+    addTask: addTask,
+    getTaskInformation: getTaskInformation
+};
---
+++ src (Updated Version)/util/deleteQueries.js
@@ -0,0 +1,34 @@
+// Import the database connection
+const pool = require('../config/database');
+
+const deleteCalendar = async (calendarID, userID) => {
+    try {
+        // SQL delete query to remove the calendar based on CalendarID and UserID
+        const sql = 'DELETE FROM Calendars WHERE CalendarID = ? AND UserID = ?';
+        const [result] = await pool.execute(sql, [calendarID, userID]);
+        return result;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteTask = async (taskID, calendarID) => {
+    try {
+        // SQL delete query to remove tasks based on TaskID and CalendarID
+        const sql = 'DELETE FROM Task WHERE TaskID = ? AND CalendarID = ?';
+        const [rows, fields] = await pool.execute(sql, [taskID, calendarID]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+// Export the deleteCalendar function
+module.exports = {
+    deleteCalendar,
+    deleteTask
+};
+
+
+
