13c383da2089df14b561e190f03a531b5f0ce22d
veronicalupinacci <116969384+veronicalupinacci@users.noreply.github.com>
2023-12-17 12:00:41 -0500

    Final code

---
+++ src/util/shareCalendar.js
@@ -0,0 +1,96 @@
+// Import the database connection
+const pool = require('../config/database');
+
+// Function to share a calendar with another user.
+const shareCalendarDB = async (userID, calendarID, sharedWithID, permissionLevel) => {
+    try {
+         // SQL insert into UserSharing based on UserID, CalendarID, SharedWithID and  PermissionLevel. 
+        const sql = "INSERT INTO UserSharing (UserID, CalendarID, SharedWithID, PermissionLevel) VALUES (?, ?, ?, ?)";
+        const [rows] = await pool.execute(sql, [userID, calendarID, sharedWithID, permissionLevel]);
+        return rows;
+    } catch (error) {
+        console.error('Error sharing calendar:', error);
+        throw error;
+    }
+};
+
+// Function to check the is Calendar Owned By User
+const isCalendarOwnedByUser = async (userID, calendarID) => {
+    try {
+        // SQL query selects CalendarID based on UserID and calendarID
+        const sql = 'SELECT CalendarID FROM Calendars WHERE UserID = ? AND CalendarID = ?';
+        const [rows] = await pool.execute(sql, [userID, calendarID]);
+
+        // Check if there's a match for the provided UserID and CalendarID
+        return rows.length > 0;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const retrieveUserIDByEmail = async (email) => {
+    try {
+        // SQL query selects UserID from UserRegistration table based on Email
+        const sql = 'SELECT UserID FROM UserRegistration WHERE Email = ?';
+        const [rows] = await pool.execute(sql, [email]);
+        if (rows.length === 1) {
+            return rows[0].UserID;
+        }
+    } catch (error) {
+        console.error('Error retrieve UserID by email:', error);
+        throw error;
+    }
+};
+
+const retrieveCalendarIDByName = async (userID, calendarName) => {
+    try {
+         // SQL query selects CalendarID based on UserID and calendarName
+         const sql = 'SELECT CalendarID FROM Calendars WHERE UserID = ? and CalendarName = ? ';
+         const [rows] = await pool.execute(sql, [userID, calendarName]);
+         if (rows.length === 1) {
+            return rows[0].CalendarID; 
+         }
+    } catch (error) {
+        console.error('Error retrieve calendarID by calendarName:', error);
+        throw error;
+    }
+};
+
+// Function to retrieve shared Calendar Names by UserID
+const retrievesSharedCalName = async (userID) => {
+    try {
+        // SQL query to retrieve Calendar Names where CalendarID from Calendars matches UserSharing's CalendarID and UserID matches SharedWithID
+        const sql = ` SELECT Calendars.CalendarName
+            FROM Calendars
+            INNER JOIN UserSharing ON Calendars.CalendarID = UserSharing.CalendarID
+            WHERE UserSharing.SharedWithID = ? `;
+        const [rows] = await pool.execute(sql, [userID]);
+        // Extracting the CalendarName only from each row and returning an array of shared calendar names
+        return rows.map(row => row.CalendarName);
+    } catch (error) {
+        throw error;
+    }
+};
+
+const isCalendarSharedWithUser = async (loggedInUserID, calendarID) => {
+    try {
+        // Check if the calendar is shared with the logged-in user
+        const sql =  'SELECT * FROM UserSharing WHERE CalendarID = ? AND SharedWithID = ?';
+        const [rows] = await pool.execute(sql, [calendarID, loggedInUserID]);
+        // Return true if the calendar is shared with the user, false otherwise
+        return rows.length > 0;
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+// Export Functions 
+module.exports = {
+    shareCalendarDB, 
+    isCalendarOwnedByUser, 
+    retrieveUserIDByEmail,
+    retrieveCalendarIDByName,
+    retrievesSharedCalName, 
+    isCalendarSharedWithUser
+}
